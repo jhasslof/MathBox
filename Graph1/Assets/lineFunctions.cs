cfc0d0b86519e52ee5716e322b4a053bbf64f213
Janne Hasslöf <Janne@ketchdigital.com>
2021-03-03 22:03:39 +0100

    Added line animation in update function

--- Graph1/Assets/lineFunctions.cs
+++ Graph1/Assets/lineFunctions.cs
@@ -1,43 +1,110 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class lineFunctions : MonoBehaviour
 {
-
+    //Public variables can be set to game objects in the Unity Editor
     public LineRenderer lineRenderer;
     public Transform point0;
     public Transform point1;
 
-    private static int numberOfFrames = 11;
+    // Private variables are set in this script
+    private float stepSize;
+    private float t;
+    private int posIndex = 0;
+    private float updateInerval;
+    private float nextTime = 0;
+    private bool continueDrawing = true;
+    private int numberOfFrames = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        lineRenderer.positionCount = numberOfFrames;
+        // Alt 1. Draw the line at Startup, without using the Update-function.
+        //          This draw a static line
+        //DrawTheLineAtStartup();
+        //DrawTheLineAtStartupVer2();
 
-        // Draw the line at Startup
-        DrawLinerCurve();
+        // Alt 2. Use the Update-function to give new linepositions inside the Update loop.
+        //          This makes a line animation
+        InitializeUpdateLinePositions();
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        // No updates done here
+        //Alt 2
+        UpdateLinePositions();
     }
 
-    private void DrawLinerCurve()
+    private void InitializeUpdateLinePositions()
     {
-        Debug.Log("Run DrawLinerCurve");
-        point0.position = new Vector3(-5, 1, 0);
-        point1.position = new Vector3(5, 1, 0);
+        // Here we have our game objects as input to the script in the Unity editor.
+        // If you don't have the game objects as input parmeters to the script you can find your objects like this
+        //
+        //point0 = GameObject.Find("BluePoint").transform;
+        //point1 = GameObject.Find("RedPoint").transform;
+        //lineRenderer = GameObject.FindObjectsOfType<LineRenderer>().Single( r => r.name == "Line");
 
+        t = 0.0f;
+        updateInerval = 0.2f; // <-- We delay update to have time to see what happens on screen
+        stepSize = (1 / (float)60);
+        nextTime = 0;
+        lineRenderer.positionCount = 0;
+    }
+
+    private void UpdateLinePositions()
+    {
+        if (Time.time >= nextTime)
+        {
+            Debug.Log("Update @" + Time.time);
+
+            if (continueDrawing)
+            {
+                Vector3 pos = CalculateLinePoint(t, point0.position, point1.position);
+                lineRenderer.positionCount = lineRenderer.positionCount + 1;
+                Debug.Log(lineRenderer.positionCount);
+
+                lineRenderer.SetPosition(posIndex, pos);
+                posIndex += 1;
+                t += stepSize;
+                if (t > 1)
+                {
+                    // Render last position
+                    t = 1;
+                    pos = CalculateLinePoint(t, point0.position, point1.position);
+                    lineRenderer.positionCount = lineRenderer.positionCount + 1;
+                    Debug.Log("Last position = " + lineRenderer.positionCount);
+
+                    lineRenderer.SetPosition(posIndex, pos);
+                    continueDrawing = false;
+                }
+            }
+            nextTime += updateInerval;
+        }
+    }
+
+    private void DrawTheLineAtStartup()
+    {
+        Debug.Log("Run DrawTheLineAtStartup");
+
+        //-------------------------------------------------------------
         //frame                     |01|02|03|04|05|06|07|08|09|10|11 |
         //percentMovedFromP0ToP1    |00|10|20|30|40|50|60|70|80|90|100|
         //X-positions               |-5|-4|-3|-2|-1|00|+1|+2|+3|+4|+5 |
         //indexInVector             |00|01|02|03|04|05|06|07|08|09|10 |
+        //-------------------------------------------------------------
+        numberOfFrames = 11;
+
+        //Line ends
+        point0.position = new Vector3(-5, 1, 0);
+        point1.position = new Vector3(5, 1, 0);
+
+        //Initialize line positions
+        lineRenderer.positionCount = numberOfFrames;
         Vector3[] positions = new Vector3[numberOfFrames];
 
         // For each frame, calculate position for percent moved from P0 to P1
@@ -59,16 +126,24 @@ public class lineFunctions : MonoBehaviour
         lineRenderer.SetPositions(positions);
     }
 
-    private void DrawLinerCurve2()
+    private void DrawTheLineAtStartupVer2()
     {
-        Debug.Log("Run DrawLinerCurve2");
-        point0.position = new Vector3(-5, 1, 0);
-        point1.position = new Vector3(5, 1, 0);
+        Debug.Log("Run DrawTheLineAtStartupVer2");
 
+        //-------------------------------------------------------------
         //frame                     |01|02|03|04|05|06|07|08|09|10|11 |
         //percentMovedFromP0ToP1    |00|10|20|30|40|50|60|70|80|90|100|
         //X-positions               |-5|-4|-3|-2|-1|00|+1|+2|+3|+4|+5 |
         //indexInVector             |00|01|02|03|04|05|06|07|08|09|10 |
+        //-------------------------------------------------------------
+        numberOfFrames = 11;
+
+        //Line ends
+        point0.position = new Vector3(-5, 1, 0);
+        point1.position = new Vector3(5, 1, 0);
+
+        //Initialize line positions
+        lineRenderer.positionCount = numberOfFrames;
         Vector3[] positions = new Vector3[numberOfFrames];
 
         // For each frame, calculate position for percent moved from P0 to P1
